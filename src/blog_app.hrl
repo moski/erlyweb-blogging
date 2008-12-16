8f8f14571f77b944d7e2b42b3cf92b19e9a2c901
Monther AbuShaikh <abushaikh@gmail.com>
2008-12-17 00:19:01 +0200

    adding the confiuration file

---
+++ src/blog_app.hrl
@@ -0,0 +1,7 @@
+-define(DB_HOSTNAME, "localhost").
+-define(DB_USERNAME, "erlang").
+-define(DB_PASSWORD, "erlang").
+-define(DB_DATABASE, "blog").
+-define(DB_POOL_SIZE, 50).
+%-define(APP_PATH, "/home/yariv/apps//twoorl").
+-define(APP_PATH, "/Users/Moski/erlang/apps/blog").
