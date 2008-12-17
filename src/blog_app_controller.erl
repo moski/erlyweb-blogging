8d0d760974b1e5e734b76f7eff85b340cd1063b2
Monther AbuShaikh <abushaikh@gmail.com>
2008-12-17 22:17:20 +0200

    committing default url changes and few bug fixed

--- src/blog_app_controller.erl
+++ src/blog_app_controller.erl
@@ -2,9 +2,31 @@
 -export([hook/1]).
 
 hook(A) ->
-	{phased, {ewc, A},
-		fun(_Ewc, Data, _PhasedVars) ->
-			{ewc, html_container, 
-			[A,  {ewc, main_layout, [A, {data, Data}]}]
-			}
-		end}.
+	A1= normalize_appmoddata(A),
+	
+	
+	Ewc = erlyweb:get_initial_ewc({ewc, A1}), 
+	case Ewc of 
+		{page,_}=Page -> 
+			Page; 
+		_ -> 
+			{phased, Ewc, fun(_Ewc, Data,_PhasedVars) -> 
+				{ewc, html_container, 
+				[A,  {ewc, main_layout, [A, {data, Data}]}]
+				}
+			end} 
+	end.
+		
+%%%% Took and modified from http://github.com/yariv/twoorl/tree/master/src/twoorl_app_controller.erl	
+normalize_appmoddata(A) ->
+  Val1 =
+  	case yaws_arg:appmoddata(A) of
+   		[] ->
+			io:fwrite("Hello world!~n", []),
+    		"/entry";
+   		Val = [$/ | _] ->
+    		Val;
+   		Val ->
+    		[$/ | Val]
+  	end,
+    yaws_arg:appmoddata(A, Val1).
